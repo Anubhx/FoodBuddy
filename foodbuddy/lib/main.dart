f9a5a55c4e0f9294e733c6405b84a528bae519c3
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-04-29 18:06:47 +0530

    Importing Colors, Constants and Assets  Setup Screen Utils, Hook GetX and Setup Entrypoint

--- foodbuddy/lib/main.dart
+++ foodbuddy/lib/main.dart
@@ -1,37 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodbuddy/constants/constants.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (context , child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'FoodBuddy',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+           scaffoldBackgroundColor: kOffWhite,
+           iconTheme:const IconThemeData(color: kDark),
+           primarySwatch: Colors.grey,
+            ),
+          home: child,
+        );
+      },
+      child: const MyHomePage(title: 'First Method'),
     );
   }
 }
