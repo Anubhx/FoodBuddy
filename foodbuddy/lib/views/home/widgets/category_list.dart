3fee60546e0b50be48202fa45a8a09446fcd1ed6
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-05-01 23:45:58 +0530

    Update CategoryList widget in category_list.dart

--- foodbuddy/lib/views/home/widgets/category_list.dart
+++ foodbuddy/lib/views/home/widgets/category_list.dart
@@ -3,15 +3,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodbuddy/common/app_style.dart';
 import 'package:foodbuddy/common/reusable_text.dart';
 import 'package:foodbuddy/constants/constants.dart';
+import 'package:foodbuddy/constants/controllers/category_controller.dart';
 import 'package:foodbuddy/constants/uidata.dart';
+import 'package:foodbuddy/views/categories/all_categories.dart';
+import 'package:get/get.dart';
 
 class CategoryList extends StatelessWidget {
   const CategoryList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(CategoryController());
     return Container(
-      height: 80.h,
+      height: 75.h,
       padding: EdgeInsets.only(left: 12.w, top: 10.h),
       child: ListView(
         scrollDirection: Axis.horizontal,
@@ -19,21 +23,36 @@ class CategoryList extends StatelessWidget {
           var category = categories[i];
           return GestureDetector(
             onTap: () {
-              
+              if (controller.categoryValue == category['_id']) {
+                controller.updateCategory = ' ';
+                controller.updateTitle = ' ';
+              } else if (category['value'] == 'more') {
+                Get.to(
+                  () => const AllCategories(),
+                  transition: Transition.fadeIn,
+                  duration: const Duration(milliseconds: 900),
+                );
+              } else {
+                controller.updateCategory = category['_id'];
+                controller.updateTitle = category['title'];
+              }
             },
-            child: Container(
+            child: Obx(() => Container(
               margin: EdgeInsets.only(right: 5.w),
               padding: EdgeInsets.only(top: 4.h),
               width: width * 0.19,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.r),
-                border: Border.all(color: kOffWhite, width: .5.w),
+                border: Border.all(color: controller.categoryValue == category['_id'] ? kSecondary: kOffWhite, width: .5.w),
               ),
               child: Column(
                 children: [
                   SizedBox(
                     height: 35.h,
-                    child: Image.network(category['imageUrl'] , fit: BoxFit.contain, ),
+                    child: Image.network(
+                      category['imageUrl'],
+                      fit: BoxFit.contain,
+                    ),
                   ),
                   ReusableText(
                     text: category['title'],
@@ -41,8 +60,7 @@ class CategoryList extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-          );
+            ), ));
         }),
       ),
     );
